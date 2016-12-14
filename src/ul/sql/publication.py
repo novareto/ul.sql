c2f6d68a76ad967949cc1685da286088eca36b3d
Souheil CHELFOUH <trollfot@gmail.com>
2016-12-14 12:27:52 +0100

    Using now the configuration object (namedtuple) instead of the classic kwargs

--- src/ul/sql/publication.py
+++ src/ul/sql/publication.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
 from cromlech.sqlalchemy import create_and_register_engine, get_session
 from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
 from ul.browser.decorators import with_zcml, with_i18n
@@ -10,6 +11,7 @@ from .decorators import transaction_sql, sql_storage
 class SQLPublication(Publication):
     """Publication Mixin
     """
+    check_configuration = None  # None or list of Interface
 
     def setup_database(self, engine):
         raise NotImplementedError
@@ -34,33 +36,42 @@ class SQLPublication(Publication):
 
         if store_root is not None:
             fs_store = HttpExposedFileSystemStore(store_root, store_prefix)
-            app = cls(session_key, engine, name, fs_store)
-            return fs_store.wsgi_middleware(app)
         else:
             fs_store = None
-            return cls(session_key, engine, name, fs_store)
 
-    def __init__(self, session_key, engine, name, fs_store=None):
-        self.name = name
-        self.session_key = session_key
-        self.engine = engine
-        self.fs_store = fs_store
+        # Configuration object
+        factory = namedtuple('Configuration', ('session_key', 'engine', 'name', 'fs_store'))
+        configuration = factory(session_key, engine, name, fs_store)
+        if cls.check_configuration is not None:
+            errors = []
+            for iface in cls.check_configuration:
+                errors.extends(getValidationErrors(iface, configuration))
+            if errors:
+                raise RuntimeError('Errors occured: %s' % ', '.join(errors))
+
+        app = cls(configuration)
+
+        if store_root is not None:
+            return fs_store.wsgi_middleware(app)
+        return app
+
+    def __init__(self, configuration):
         self.publish = self.get_publisher()
-        self.setup_database(engine)
+        self.configuration = configuration
 
     def __runner__(self, func):
-        @transaction_sql(self.engine)
-        @sql_storage(self.fs_store)
+        @transaction_sql(self.configuration.engine)
+        @sql_storage(self.configuration.fs_store)
         def run(*args):
             return func(*args)
         return run
 
     def __interact__(self, banner=u'', **namespace):
 
-        @transaction_sql(self.engine)
-        @sql_storage(self.fs_store)
+        @transaction_sql(self.configuration.engine)
+        @sql_storage(self.configuration.fs_store)
         def shell():
-            session = get_session(self.name)
+            session = get_session(self.configuration.name)
             namespace['sql_session'] = session
             return super(SQLPublication, self).__interact__(
                 banner=u'', **namespace)
@@ -69,8 +80,8 @@ class SQLPublication(Publication):
 
     def __call__(self, environ, start_response):
 
-        @transaction_sql(self.engine)
-        @sql_storage(self.fs_store)
+        @transaction_sql(self.configuration.engine)
+        @sql_storage(self.configuration.fs_store)
         def publish(environ, start_response):
             return super(SQLPublication, self).__call__(
                 environ, start_response)
